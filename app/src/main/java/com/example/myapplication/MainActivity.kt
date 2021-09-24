997a6a139f7ff7521780eecbe86391a64ba6cc8f
DmitryWork <nekrasov.di91@gmail.com>
2021-09-24 13:32:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun onClickToast(view: View) {
+        Toast.makeText(this, "Вперед!", Toast.LENGTH_SHORT).show()
+    }
+}
